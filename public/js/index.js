b658ef88b51a873bfadfc775495df9155558bf4a
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-17 21:42:07 +0530

    Add Messages to the screen

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,27 @@ socket.on('connect', function ()  { //for the client side.
 
  socket.on('newMessage', function (message) {
    console.log('New Message', message);
+
+   var li = jQuery('<li></li>');
+   li.text(`${message.from}: ${message.text}`);
+
+   jQuery('#messages').append(li);
  });
 
-socket.emit('createMessage', {
-    from: 'Harvey',
-    text: 'Hey Mike!'
-  }, function () {
-    console.log('Got it.');
+// socket.emit('createMessage', {
+//     from: 'Harvey',
+//     text: 'Hey Mike!'
+//   }, function () {
+//     console.log('Got it.');
+//   });
+
+  jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+      from:'User',
+      text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
   });
