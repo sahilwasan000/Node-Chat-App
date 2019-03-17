de525fb596b6314109a21393de184fb17bf31796
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-18 02:39:52 +0530

    Add User location

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 
 var expect = require('expect');
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
   it('should generate the correct message', () => {
@@ -13,7 +13,18 @@ describe('generateMessage', () => {
       from: from, //({form, text})
       text: text
       });
+  });
+});
 
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Laura';
+    var latitude = 55;
+    var longitude = -59;
+    var url = 'https://www.google.com/maps?q=55,-59';
+    var message = generateLocationMessage(from, latitude, longitude);
 
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,url});
   });
 });
