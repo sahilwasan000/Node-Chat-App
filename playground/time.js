847d80c198b1e43f96e2ff2fbf0759a75c33e428
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-19 00:06:19 +0530

    Add Time Stamp

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+// var date = new Date();
+// console.log(date.getMonth());
+
+var moment = require('moment');
+var date = moment(); //library for displaying timestamp.
+
+console.log(date.format('LT'));
